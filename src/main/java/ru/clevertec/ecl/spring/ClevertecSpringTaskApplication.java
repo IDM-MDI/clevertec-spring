3a21076ef56f1edda96e988174d1e2552176e706
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-24 12:49:46 +0300

    Initial commit

---
+++ src/main/java/ru/clevertec/ecl/spring/ClevertecSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.ecl.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecSpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClevertecSpringTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clevertec/ecl/spring/ClevertecSpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.ecl.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecSpringTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
