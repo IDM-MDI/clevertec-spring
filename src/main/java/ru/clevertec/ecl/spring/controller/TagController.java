3785c5f47e9e83e091b91fe13c8521dc9f41e1c4
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-25 13:05:03 +0300

    add controller

--- src/main/java/ru/clevertec/ecl/spring/controller/TagController.java
+++ src/main/java/ru/clevertec/ecl/spring/controller/TagController.java
@@ -1,8 +1,13 @@
 package ru.clevertec.ecl.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.clevertec.ecl.spring.model.TagDTO;
 
 @RestController
 @RequestMapping("/tags")
@@ -10,5 +15,18 @@ public class TagController {
     @GetMapping
     public String findTags() {
         return "This method should return list of tags";
+    }
+    @GetMapping("/{id}")
+    public String findTag(@PathVariable long id) {
+        return "This method should return tag by id";
+    }
+    @PostMapping
+    public String saveTag(@RequestBody TagDTO tag) {
+        return "This method should save tag and return it";
+    }
+    @PatchMapping("/{id}")
+    public String updateTag(@PathVariable long id,
+                            @RequestBody TagDTO tag) {
+        return "This method should update tag and return it";
     }
 }
