3bc82e2502ace4897d6af757a9a4b328627882f3
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-25 13:07:57 +0300

    add controller

---
+++ src/main/java/ru/clevertec/ecl/spring/controller/GiftCertificateController.java
@@ -0,0 +1,33 @@
+package ru.clevertec.ecl.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.clevertec.ecl.spring.model.GiftCertificateDTO;
+import ru.clevertec.ecl.spring.model.TagDTO;
+
+@RestController
+@RequestMapping("/api/v1/gifts")
+public class GiftCertificateController {
+    @GetMapping
+    public String findGifts() {
+        return "This method should return list of gifts";
+    }
+    @GetMapping("/{id}")
+    public String findGift(@PathVariable long id) {
+        return "This method should return gift by id";
+    }
+    @PostMapping
+    public String saveGift(@RequestBody GiftCertificateDTO gift) {
+        return "This method should save gift and return it";
+    }
+    @PatchMapping("/{id}")
+    public String updateGift(@PathVariable long id,
+                             @RequestBody GiftCertificateDTO gift) {
+        return "This method should update gift and return it";
+    }
+}
