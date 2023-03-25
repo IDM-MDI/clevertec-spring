317ebddacfb1761527248ff9c19d73c47514a512
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-25 13:24:39 +0300

    add hibernate validator

--- src/main/java/ru/clevertec/ecl/spring/model/GiftCertificateDTO.java
+++ src/main/java/ru/clevertec/ecl/spring/model/GiftCertificateDTO.java
@@ -1,18 +1,37 @@
 package ru.clevertec.ecl.spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import ru.clevertec.ecl.spring.entity.Tag;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@Builder
+@AllArgsConstructor
 public class GiftCertificateDTO {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private long id;
+    @NotBlank
+    @Length(min = 2)
     private String name;
+    @NotBlank
+    @Length(min = 3)
     private String description;
-    private BigDecimal price;
+    @Min(1)
+    private Double price;
+    @Min(1)
     private long duration;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime create_date;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime update_date;
     private List<Tag> tags;
 }
--- src/main/java/ru/clevertec/ecl/spring/model/TagDTO.java
+++ src/main/java/ru/clevertec/ecl/spring/model/TagDTO.java
@@ -1,14 +1,21 @@
 package ru.clevertec.ecl.spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class TagDTO {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private long id;
+    @NotBlank
+    @Length(min = 2)
     private String name;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String status;
 }
